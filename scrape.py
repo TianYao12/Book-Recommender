178e3cc384d04565345815a42d0386499ceea471
TianYao12 <110309420+TianYao12@users.noreply.github.com>
2024-01-20 14:39:51 -0500

    commit example

--- scrape.py
+++ scrape.py
@@ -2,9 +2,20 @@ import os
 from bs4 import BeautifulSoup
 import requests
 
-def get_games():
+def main():
     book_url = "https://www.goodreads.com/book/show/"
-    req = requests.get(url=book_url).json()
-    for i in range(2000):
+    for i in range(20):
+        url = book_url + str(i)
+        res = requests.get(url)
+        
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.text, 'html.parser')
+            links = soup.select(".Text__title1")
+            print(links)
+        else:
+            print(f"Error: Unable to fetch the page. Status code: {res.status_code}")
 
+
+if __name__ == '__main__':
+    main()
 

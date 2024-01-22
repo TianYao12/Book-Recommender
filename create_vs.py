40e8b4c509b6ab16ec662ca561f95f765a070c18
TianYao12 <110309420+TianYao12@users.noreply.github.com>
2024-01-22 11:38:22 -0500

    converted book info to vectorstore

---
+++ create_vs.py
@@ -0,0 +1,67 @@
+import os
+import time
+import json
+from tqdm import tqdm
+from dotenv import load_dotenv
+from langchain.vectorstores.faiss import FAISS
+from langchain.docstore.document import Document
+from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+load_dotenv(".env.local")
+KEY = os.environ["HUGGINGFACEHUB_API_TOKEN"]
+
+# turns web-scrapped JSON documents into LangChain documents
+def get_json_data() -> list:
+    books = json.load(open("data/goodreads_books.json", "r")
+                      )  # get json data from file
+    documents = []
+
+    for b in books:
+        documents.append(Document(page_content=b["description"],  # append each book's information to documents list
+                                  metadata={
+            "title": b["title"],
+            "authors": b["authors"],
+            "image": b["image_url"],
+            "url": b["url"]
+        }))
+
+    print(
+        f"Loaded {len(documents)} documents\n Next we will split this into chunks")
+
+    # perhaps switch up these numbers later
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200)
+    chunks = splitter.split_documents(documents)
+    return chunks
+
+# turns document chunks into vectore stores
+
+
+def get_vector_store(chunks: list, model: HuggingFaceInferenceAPIEmbeddings) -> FAISS:
+    vsdatabase = []
+    for i in tqdm(range(0, len(chunks), 400)):
+        partial_vs = FAISS.from_documents(chunks[i: i+400], model) # 400 at a limit limit with Huggingface API
+        vsdatabase.append(partial_vs)
+        time.sleep(3)
+
+    # Merge partial vector stores into one large vector store
+    for partial_vs in tqdm(vsdatabase[1:]):
+        vsdatabase[0].merge_from(partial_vs)
+    partial_vs.save_local("data/complete_vector_store")
+
+
+def main():
+    # load embeddings model
+    model = HuggingFaceInferenceAPIEmbeddings(
+        api_key=KEY, model_name="sentence-transformers/all-MiniLM-l6-v2"
+    )
+
+    # get vectorstore
+    chunks = get_json_data()
+    get_vector_store(chunks, model)
+
+
+if __name__ == "__main__":
+    main()
--- scrape.py
+++ scrape.py
@@ -1,21 +1,68 @@
-import os
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+import json
+import numpy as np
+import time
 
-def main():
-    book_url = "https://www.goodreads.com/book/show/"
-    for i in range(20):
-        url = book_url + str(i)
-        res = requests.get(url)
-        
-        if res.status_code == 200:
-            soup = BeautifulSoup(res.text, 'html.parser')
-            links = soup.select(".Text__title1")
-            print(links)
-        else:
-            print(f"Error: Unable to fetch the page. Status code: {res.status_code}")
+def scrape_book_info(book_id):
+    url = f'https://www.goodreads.com/book/show/{book_id}'
+
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Extracting information with error handling
+    title_element = soup.select_one('h1[data-testid="bookTitle"]')
+    image_element = soup.select_one('img[class="ResponsiveImage"]')
+    description_element = soup.select_one('span[class="Formatted"]')
+    author_elements = soup.find_all(
+        'span', class_='ContributorLink__name', attrs={'data-testid': 'name'})
+
+    # Check if elements are present before extracting text or attributes
+    title = title_element.get_text(strip=True) if title_element else None
+    image_url = image_element['src'] if image_element and 'src' in image_element.attrs else None
+    description = description_element.get_text(
+        strip=True) if description_element else None
+    authors = list(set(author_element.get_text(strip=True) for author_element in author_elements))
 
 
-if __name__ == '__main__':
-    main()
+    book_info = {
+        'title': title,
+        'image_url': image_url,
+        'description': description,
+        'url': url,
+        'authors': authors
+    }
+    return book_info
 
+
+def scrape_books(start_id, end_id, step):
+    all_books_info = []
+
+    for book_id in range(start_id, end_id, step):
+        book_info = scrape_book_info(book_id)
+        if book_info and book_info['title'] and book_info['image_url'] and book_info['description']:
+            all_books_info.append(book_info)
+        lag = np.random.uniform(low=1, high=3)
+        print(f'...waiting {round(lag,1)} seconds...')
+        time.sleep(lag)
+    return all_books_info
+
+
+if __name__ == "__main__":
+    start_book_id = 1
+    end_book_id = 10000
+    step = 10
+
+    book_info_list = scrape_books(start_book_id, end_book_id, step)
+
+    # print(book_info_list)
+
+    # Save the scraped data to a JSON file
+    with open('goodreads_books.json', 'w') as f:
+        json.dump(book_info_list, f, indent=2)
